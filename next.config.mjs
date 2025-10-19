3bb3f5c3bd899618fde3e2d972bec5d34a7e6bfb
Mshahzaib123 <88280693+Mshahzaib123@users.noreply.github.com>
2025-10-19 23:41:07 +0500

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,11 @@ const nextConfig = {
             },
             {
                 protocol: 'https',
+                hostname: 'cdn.myanimelist.net',
+                pathname: '/images/**',
+            },
+            {
+                protocol: 'https',
                 hostname: 'via.placeholder.com',
             },
         ],
